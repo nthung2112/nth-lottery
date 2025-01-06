84dfc02955145fe9055c5d5372d098b82f0c8fd4
Hung Nguyen <nthung2112@gmail.com>
2025-01-06 23:16:33 +0700

    Update default music

--- src/store/data.ts
+++ src/store/data.ts
@@ -545,59 +545,19 @@ export const defaultPersonList = <IPersonConfig[]>[
 
 export const defaultMusicList = [
   {
-    id: "Geoff Knorr - China (The Industrial Era).ogg" + new Date().getTime().toString(),
-    name: "Geoff Knorr - China (The Industrial Era).ogg",
-    url: "https://1kw20.fun/resource/audio/Geoff Knorr - China (The Industrial Era).ogg",
+    id: "default.mp3" + new Date().getTime().toString(),
+    name: "Nhac Mac Dinh",
+    url: "/music/default.mp3",
   },
   {
-    id: "Geoff Knorr&Phill Boucher - China (The Atomic Era).ogg" + new Date().getTime().toString(),
-    name: "Geoff Knorr&Phill Boucher - China (The Atomic Era).ogg",
-    url: "https://1kw20.fun/resource/audio/Geoff Knorr&Phill Boucher - China (The Atomic Era).ogg",
+    id: "xoso.mp3" + new Date().getTime().toString(),
+    name: "Nhac Xo So",
+    url: "/music/xoso.mp3",
   },
   {
-    id: "Radetzky March.mp3" + new Date().getTime().toString(),
-    name: "Radetzky March.mp3",
-    url: "https://1kw20.fun/resource/audio/Radetzky March.mp3",
-  },
-  {
-    id: "Shanghai.mp3" + new Date().getTime().toString(),
-    name: "Shanghai.mp3",
-    url: "https://1kw20.fun/resource/audio/Shanghai.mp3",
-  },
-  {
-    id: "Waltz No.2.mp3" + new Date().getTime().toString(),
-    name: "Waltz No.2.mp3",
-    url: "https://1kw20.fun/resource/audio/Waltz No.2.mp3",
-  },
-  {
-    id: "WildChinaTheme.mp3" + new Date().getTime().toString(),
-    name: "WildChinaTheme.mp3",
-    url: "https://1kw20.fun/resource/audio/WildChinaTheme.mp3",
-  },
-  {
-    id: "边程&房东的猫 - 美好事物-再遇少年.ogg" + new Date().getTime().toString(),
-    name: "边程&房东的猫 - 美好事物-再遇少年.ogg",
-    url: "https://1kw20.fun/resource/audio/边程&房东的猫 - 美好事物-再遇少年.ogg",
-  },
-  {
-    id: "大乔小乔 - 相见难别亦难.ogg" + new Date().getTime().toString(),
-    name: "大乔小乔 - 相见难别亦难.ogg",
-    url: "https://1kw20.fun/resource/audio/大乔小乔 - 相见难别亦难.ogg",
-  },
-  {
-    id: "你要跳舞吗-新裤子.mp3" + new Date().getTime().toString(),
-    name: "你要跳舞吗-新裤子.mp3",
-    url: "https://1kw20.fun/resource/audio/你要跳舞吗-新裤子.mp3",
-  },
-  {
-    id: "生命-声音玩具.mp3" + new Date().getTime().toString(),
-    name: "生命-声音玩具.mp3",
-    url: "https://1kw20.fun/resource/audio/生命-声音玩具.mp3",
-  },
-  {
-    id: "与非门 - Happy New Year.ogg" + new Date().getTime().toString(),
-    name: "与非门 - Happy New Year.ogg",
-    url: "https://1kw20.fun/resource/audio/与非门 - Happy New Year.ogg",
+    id: "xuan.mp3" + new Date().getTime().toString(),
+    name: "Nhac Xuan",
+    url: "/music/xuan.mp3",
   },
 ];
 
@@ -711,21 +671,21 @@ export const defaultPrizeList = <IPrizeConfig[]>[
 
 export const defaultCurrentPrize = <IPrizeConfig>{
   id: "001",
-  name: "三等奖",
+  name: "Third prize",
   sort: 1,
   isAll: false,
   count: 12,
   isUsedCount: 0,
   picture: {
     id: "2",
-    name: "三等奖",
+    name: "Third prize",
     url: "https://1kw20.fun/resource/image/image3.png",
   },
   separateCount: {
     enable: true,
     countList: [],
   },
-  desc: "三等奖",
+  desc: "Third prize",
   isShow: true,
   isUsed: false,
   frequency: 1,
@@ -781,6 +741,7 @@ export const defaultImageList = [
   },
 ];
 
+// 2025
 export const defaultPatternList = [
   21, 38, 55, 54, 53, 70, 87, 88, 89, 23, 40, 57, 74, 91, 92, 93, 76, 59, 42, 25, 24, 27, 28, 29,
   46, 63, 62, 61, 78, 95, 96, 97, 20, 19, 31, 48, 65, 66, 67, 84, 101, 100, 99, 32, 33,
